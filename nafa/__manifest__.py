d13d928d846e7721402d2aceb1e794d955e4bccc
kjonathan1 <jonathankabre1@gmail.com>
2024-07-23 12:12:42 +0100

    initialisation of nafa module

---
+++ nafa/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'nafa',
+    'version': '1.0',
+    'category': 'Custom',
+    'summary': 'Backend sercice',
+    'description': """
+        Backend sercice
+    """,
+    'depends': [
+        'base',
+        'portal',
+        'utm',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/res_groups.xml',
+        # 'security/ir_rules.xml',
+
+        # 'report/account_invoice_report_views.xml',
+
+        # 'data/ir_cron.xml',
+        'data/ir_sequence_data.xml',
+
+        # Define views before their references
+        'views/job.xml',
+
+        'views/menu.xml',  # Last because referencing actions defined in previous files
+    ],
+    'demo': [],
+    'installable': True,
+    'license': 'LGPL-3',
+}
---
+++ nafa/models/job.py
@@ -0,0 +1,51 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import defaultdict
+from datetime import timedelta
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.fields import Command
+from odoo.osv import expression
+from odoo.tools import float_is_zero, format_amount, format_date, html_keep_url, is_html_empty
+from odoo.tools.sql import create_index
+
+from odoo.addons.payment import utils as payment_utils
+
+INVOICE_STATUS = [
+    ('upselling', 'Upselling Opportunity'),
+    ('invoiced', 'Fully Invoiced'),
+    ('to invoice', 'To Invoice'),
+    ('no', 'Nothing to Invoice')
+]
+
+SALE_ORDER_STATE = [
+    ('draft', "Quotation"),
+    ('sent', "Quotation Sent"),
+    ('sale', "Sales Order"),
+    ('cancel', "Cancelled"),
+]
+
+
+
+class NafaJob(models.Model):
+    _name = "nafa.job"
+    _description = "Job"
+        
+
+    @api.model
+    def create(self, vals):
+        sequence = self.env['ir.sequence'].next_by_code('nafa.job') or _('New')
+        vals['name'] = sequence
+        result = super(NafaJob, self).create(vals)
+        return result
+
+    start_date = fields.Date(string="Openning date", required=True, default=fields.Date.today)
+    end_date = fields.Date(string="Closing date", required=True,)
+    name = fields.Char(string="Reference" , readonly=True)
+    position = fields.Char(string="Position")
+    experience = fields.Char(string="Experience")
+    description = fields.Char(string="Description")
+    category = fields.Selection([('job','Job'), ('internship','Internship'),('trainig','Trainig')], string='Category', readonly=True, default='job')
+    state = fields.Selection([('draft','Draft'), ('open','Open'),('close','Close')], string='Etat', readonly=True, default='draft')
+       
